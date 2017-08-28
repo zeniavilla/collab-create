977b3428efc1f14c50bbd29960e37805a55b0755
Zenia <zeniavilla@hotmail.com>
2017-08-28 16:07:30 -0700

    Change /sign_up route to use UserNew

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import Services from '../containers/Services';
 import NavBar from './NavBar';
 import Footer from './Footer';
-import UserSignUpForm from './UserSignUpForm';
+import UserNew from '../containers/UserNew';
 import UserProfileEdit from '../containers/UserProfileEdit';
 
 class App extends Component {
@@ -16,7 +16,7 @@ class App extends Component {
           <div id="wrapper">
             <NavBar />
             <Route path="/services" component={Services} />
-            <Route path="/sign_up" component={UserSignUpForm} />
+            <Route path="/sign_up" component={UserNew} />
             <Route path="/profile/:user_id" component={UserProfileEdit} />
           </div>
 
