fd5657cff520e6a9bd67d9d1751856059e15a0c8
Zenia <zeniavilla@hotmail.com>
2017-08-28 15:00:38 -0700

    Add handleOnClick and handleOnChange props to user signup form

--- client/src/components/UserSignUpForm.js
+++ client/src/components/UserSignUpForm.js
@@ -1,26 +1,36 @@
 import React from 'react';
 
-const UserSignUpForm = () =>
-  <form>
-    <div className="form-field">
-      <input type="text" id="name" placeholder="Name" />
-    </div>
+const clientId = process.env.INSTA_CLIENT_ID;
 
-    <div className="form-field">
-      <input type="email" id="email" placeholder="Email" />
-    </div>
+const UserSignUpForm = props => {
+  const { handleOnClick, handleOnChange } = props;
 
-    <div className="form-field">
-      <input type="password" id="name" placeholder="Password" />
-    </div>
+  return (
+    <div>
+      <form>
+        <div className="form-field">
+          <input type="text" id="name" placeholder="Name" />
+        </div>
 
-    <div className="form-field">
-      <input type="password" id="name" placeholder="Confirm password" />
-    </div>
+        <div className="form-field">
+          <input type="email" id="email" placeholder="Email" />
+        </div>
 
-    <div className="form-action">
-      <button className="btn-primary">Sign Up</button>
+        <div className="form-field">
+          <input type="password" id="name" placeholder="Password" />
+        </div>
+
+        <div className="form-field">
+          <input type="password" id="name" placeholder="Confirm password" />
+        </div>
+
+        <div className="form-action">
+          <button className="btn-primary">Sign Up</button>
+        </div>
+      </form>
+      <button onChange={handleOnChange} onClick={handleOnClick} className="btn-insta">Continue with Facebook</button>
     </div>
-  </form>
+  )
+}
 
 export default UserSignUpForm;
