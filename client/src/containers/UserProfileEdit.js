147aed68dacc318a61ec72dc746940a1b3bfb08a
Zenia <zeniavilla@hotmail.com>
2017-08-28 11:36:31 -0700

    Initialize UserProfilleEdit container

---
+++ client/src/containers/UserProfileEdit.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import UserProfileForm from '../components/UserProfileForm';
+
+class UserProfileEdit extends Component {
+  render() {
+    return (
+      <UserProfileForm />
+    )
+  }
+}
+
+export default UserProfileEdit;
