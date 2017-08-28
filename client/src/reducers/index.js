965ec394b2c12a454c11c9feaf1dceeb30732282
Zenia <zeniavilla@hotmail.com>
2017-08-28 15:11:32 -0700

    Add userFormData to combineReducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { servicesReducer } from './servicesReducer';
+import { userFormData } from './userFormData';
 
 export default combineReducers({
-  services: servicesReducer
+  services: servicesReducer,
+  userFormData: userFormData
 });
