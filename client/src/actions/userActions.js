5ba6810a4789508fa8ec62f74fe9374389a0313c
Zenia <zeniavilla@hotmail.com>
2017-08-28 19:34:37 -0700

    Create user actions

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,8 +1,50 @@
+const API_URL = process.env.REACT_APP_API_URL;
+
 // ** Action Creators **
 
 export const updateUserFormData = userFormData => {
   return {
     type: 'UPDATED_USER_DATA',
     userFormData
+  }
+}
+
+const addOmniauthUser = user => {
+  return {
+    type: 'CREATE_OMNIAUTH_USER',
+    user
+  }
+}
+
+const addUser = user => {
+  return {
+    type: 'CREATE_USER',
+    user
+  }
+}
+
+// ** Async Actions **
+
+export const createOmniauthUser = provider => {
+  return dispatch => {
+    return fetch(`${API_URL}/users/auth/${provider}`)
+      .then(response => response.json())
+      .then(user => dispatch(addOmniauthUser(user)))
+      .catch(error => console.log(error));
+  }
+}
+
+export const createUser = user => {
+  return dispatch => {
+    return fetch(`${API_URL}/users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ user: user })
+    })
+      .then(response => response.json())
+      .then(user => dispatch(addUser(user)))
+      .catch(error => console.log(error))
   }
 }
