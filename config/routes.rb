59ac6bee6005372a979fca6ef9edfe549b92e21a
Zenia <zeniavilla@hotmail.com>
2017-08-28 19:36:46 -0700

    Add /api scope to devise users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  scope '/api' do
+    devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  end
 
   namespace :api do
     resources :services, except: [:new, :edit]
