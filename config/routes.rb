fca48566de5ec456ba9abd0a7f9e3be965e7f7ae
Zenia <zeniavilla@hotmail.com>
2017-08-30 16:35:11 -0700

    Specify  sessions and registrations controllers in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   scope '/api' do
-    devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+    devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :sessions => 'sessions', :registrations => 'registrations' }
   end
 
   namespace :api do
