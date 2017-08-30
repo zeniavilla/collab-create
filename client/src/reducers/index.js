6ebe2569bbeb9eeead066777052c3926f86f1d4c
Zenia <zeniavilla@hotmail.com>
2017-08-30 15:12:08 -0700

    Add currentUser reducer to rootReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { servicesReducer } from './servicesReducer';
 import { userFormData } from './userFormData';
+import { usersReducer } from './usersReducer';
 
 export default combineReducers({
   services: servicesReducer,
-  userFormData: userFormData
+  userFormData: userFormData,
+  currentUser: usersReducer
 });
