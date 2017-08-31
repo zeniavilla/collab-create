3dbc13b7b24021021fd91cdc0c9cf6797f6d80a4
Zenia <zeniavilla@hotmail.com>
2017-08-31 10:57:25 -0700

    Create sign in form component

---
+++ client/src/components/SignInForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const SignInForm = props => {
+  const { handleOnChange, handleOnSubmit } = props;
+  const { email, password } = props.userFormData;
+
+  return(
+    <form onSubmit={handleOnSubmit}>
+      <div className="form-field">
+        <input type="email" id="email" placeholder="Email" onChange={handleOnChange} value={email} />
+      </div>
+
+      <div className="form-field">
+        <input type="password" id="password" placeholder="Password" onChange={handleOnChange} value={password} />
+      </div>
+
+      <div className="form-action">
+        <button className="btn-primary">Sign In</button>
+      </div>
+    </form>
+  )
+}
+
+export default SignInForm;
