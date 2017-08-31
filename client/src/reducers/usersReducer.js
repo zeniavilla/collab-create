817dbb81149c8d401b947e110eeb13816ea7bcf0
Zenia <zeniavilla@hotmail.com>
2017-08-31 14:28:26 -0700

    Add initial state to users reducer to clear current user

--- client/src/reducers/usersReducer.js
+++ client/src/reducers/usersReducer.js
@@ -1,7 +1,12 @@
-export const usersReducer = (state = [], action) => {
+const initialState = [];
+
+export const usersReducer = (state = initialState, action) => {
   switch(action.type) {
     case 'CREATE_USER':
       return action.user;
+    
+    case 'CLEAR_CURRENT_USER':
+      return initialState;
 
     default:
       return state;
