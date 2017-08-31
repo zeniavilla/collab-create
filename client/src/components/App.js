9275d9b18b2687146f12b6766f324e2fcf26f07d
Zenia <zeniavilla@hotmail.com>
2017-08-31 10:27:45 -0700

    Add NavCurrentUser container to store state.currentUser in props

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
 import Services from '../containers/Services';
-import NavBar from './NavBar';
+import NavCurrentUser from '../containers/NavCurrentUser';
 import Footer from './Footer';
 import UserNew from '../containers/UserNew';
 import UserProfileEdit from '../containers/UserProfileEdit';
@@ -14,7 +14,7 @@ class App extends Component {
       <Router>
         <div>
           <div id="wrapper">
-            <NavBar />
+            <NavCurrentUser />
             <Route path="/services" component={Services} />
             <Route path="/sign_up" component={UserNew} />
             <Route path="/profile/:user_id" component={UserProfileEdit} />
---
+++ client/src/containers/NavCurrentUser.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import NavBar from '../components/NavBar';
+
+class NavCurrentUser extends Component {
+  render() {
+    return(
+      <NavBar currentUser={this.props.currentUser} />
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return ({ currentUser: state.currentUser })
+}
+
+export default connect (mapStateToProps)(NavCurrentUser);
