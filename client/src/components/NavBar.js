bfec67b689483520a8549e64ace801336e6183c3
Zenia <zeniavilla@hotmail.com>
2017-08-31 14:58:56 -0700

    Refactor navbar conditionals for userLinks and guestLinks

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,45 +3,51 @@ import { Link } from 'react-router-dom';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
-const NavBar = props =>
-<Navbar inverse collapseOnSelect>
-  <Navbar.Header>
-    <Navbar.Brand>
-      <Link to="/">collab|create</Link>
-    </Navbar.Brand>
-    <Navbar.Toggle />
-  </Navbar.Header>
-  <Navbar.Collapse>
-    <Nav>
-      <NavItem eventKey={1} href="#">Link</NavItem>
-      <NavItem eventKey={2} href="#">Link</NavItem>
-      <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-        <MenuItem eventKey={3.1}>Action</MenuItem>
-        <MenuItem eventKey={3.2}>Another action</MenuItem>
-        <MenuItem eventKey={3.3}>Something else here</MenuItem>
-        <MenuItem divider />
-        <MenuItem eventKey={3.3}>Separated link</MenuItem>
-      </NavDropdown>
+const NavBar = props => {
+  const userLinks = (
+    <Nav pullRight>
+      <NavItem eventKey={1}>{props.currentUser.name}</NavItem>
+      <LinkContainer to="/sign_out">
+        <NavItem eventKey={2}>Sign Out</NavItem>
+      </LinkContainer>
     </Nav>
-    
-      {
-        (props.currentUser.id) 
-        ? <Nav pullRight>
-          <NavItem eventKey={1}>{props.currentUser.name}</NavItem>
-          <LinkContainer to="/sign_out">
-            <NavItem eventKey={2}>Sign Out</NavItem>
-          </LinkContainer>
-          </Nav>
-        : <Nav pullRight>
-        <LinkContainer to="/sign_in">
-          <NavItem eventKey={1}>Sign In</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/sign_up">
-          <NavItem eventKey={2}>Create Account</NavItem>
-        </LinkContainer>
+  )
+
+  const guestLinks = (
+    <Nav pullRight>
+      <LinkContainer to="/sign_in">
+        <NavItem eventKey={1}>Sign In</NavItem>
+      </LinkContainer>
+      <LinkContainer to="/sign_up">
+        <NavItem eventKey={2}>Create Account</NavItem>
+      </LinkContainer>
+    </Nav>
+  )
+
+  return (
+    <Navbar inverse collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <Link to="/">collab|create</Link>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav>
+          <NavItem eventKey={1} href="#">Link</NavItem>
+          <NavItem eventKey={2} href="#">Link</NavItem>
+          <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+            <MenuItem eventKey={3.1}>Action</MenuItem>
+            <MenuItem eventKey={3.2}>Another action</MenuItem>
+            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey={3.3}>Separated link</MenuItem>
+          </NavDropdown>
         </Nav>
-      }
-  </Navbar.Collapse>
-</Navbar>
+        { (props.currentUser.id) ? userLinks : guestLinks }
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
 
 export default NavBar;
