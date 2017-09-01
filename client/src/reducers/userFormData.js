a1c97365ff365227dcb6b7ec27b363c6673f60e2
Zenia <zeniavilla@hotmail.com>
2017-09-01 09:23:03 -0700

    Create reset user form reducer action

--- client/src/reducers/userFormData.js
+++ client/src/reducers/userFormData.js
@@ -9,6 +9,8 @@ export const userFormData = (state = initialState, action) => {
   switch(action.type) {
     case 'UPDATED_USER_DATA':
       return action.userFormData
+    case 'RESET_USER_FORM':
+      return initialState
 
     default:
       return state;
