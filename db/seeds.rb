cf854bed7952e360da996549ba1f768b0ad408a7
Zenia <zeniavilla@hotmail.com>
2017-09-06 18:36:28 -0700

    Create seed for services

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Service.destroy_all
+
+DATA = {
+  :services => [
+    'Food Styling', 'Hair', 'Makeup', 'Model', 'Photography', 'Videography', 'Wardrobe Styling'
+  ]
+}
+
+DATA[:services].each do |service|
+  new_service = Service.new
+  new_service.send("name=", service)
+  new_service.save
+end
