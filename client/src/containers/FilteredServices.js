8a6166fd628eb317de6708ffb99fd261988b0f9e
Zenia <zeniavilla@hotmail.com>
2017-09-09 20:03:39 -0700

    Create FilteredServices container

---
+++ client/src/containers/FilteredServices.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Service from '../components/Service';
+
+class FilteredServices extends Component {
+
+  render() {
+    const renderServices = this.props.services.map(service =>
+      <Service service={service} />)
+      return(
+        <div>
+        {renderServices}
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  const serviceSlug = ownProps.match.params.serviceName;
+  const serviceName = serviceSlug.split('-').map(word =>
+    `${word[0].toUpperCase()}${word.slice(1)}`
+  ).join(' ');
+  
+  return ({ services: state.services.filter(service => service.name === serviceName) })
+}
+
+export default connect(mapStateToProps)(FilteredServices);
