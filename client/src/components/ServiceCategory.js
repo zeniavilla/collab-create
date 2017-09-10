b3c4b4fc942a80f8a0e9ba81060e1be66a321ad8
Zenia <zeniavilla@hotmail.com>
2017-09-09 20:04:19 -0700

    Add link with slugified name to service cards in Service page

--- client/src/components/ServiceCategory.js
+++ client/src/components/ServiceCategory.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import { Col } from 'react-bootstrap';
 
 const ServiceCategory = props => {
   const { name } = props.service;
+  const slug = name.toLowerCase().split(' ').join('-');
 
   return (
     <div>
       <Col sm={3}>
-        <div className="service-img">
-          {name}
-        </div>
+        <Link to={`/services/${slug}`}>
+          <div className="service-img">
+            {name}
+          </div>
+        </Link>
       </Col>
     </div>
   )
