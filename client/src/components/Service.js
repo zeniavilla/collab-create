d0a740e94a8226cc61c95b1dd0899e0aaf86db8b
Zenia <zeniavilla@hotmail.com>
2017-09-10 19:49:37 -0700

    Render users in Service component

--- client/src/components/Service.js
+++ client/src/components/Service.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import UserCard from './UserCard';
+
 const Service = props => {
+  const renderUsers = props.service.users.map(user => <UserCard user={user} key={user.id} />);
+
   return (
     <div>
-      <h2>Hello</h2>
+      <h2>{props.service.name}</h2>
+      <div>{renderUsers}</div>
     </div>
   )
 }
