6676e467c0d35b8794b30eb5ea3eb4eb49de62e1
Zenia <zeniavilla@hotmail.com>
2017-09-10 19:54:38 -0700

    Create UserShow container to get user in params from server api

---
+++ client/src/containers/UserShow.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { getSelectedUserService } from '../actions/userActions';
+
+import UserPublicProfile from '../components/UserPublicProfile';
+
+class UserShow extends Component {
+  
+  componentDidMount = () => {
+    const { userId } = this.props.match.params;
+    this.props.getSelectedUserService(userId);
+  }
+
+  render() {
+    return (
+      <div>
+        <UserPublicProfile user={this.props.selectedUserService} />
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return ({ selectedUserService: state.selectedUserService })
+}
+
+export default connect(mapStateToProps, { getSelectedUserService })(UserShow);
