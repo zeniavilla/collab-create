1f5393c2c1e4486e2ad72a8af73841167e9373aa
Zenia <zeniavilla@hotmail.com>
2017-09-14 16:51:57 -0700

    Add rows and columns to PublicProfile component

--- client/src/components/UserPublicProfile.js
+++ client/src/components/UserPublicProfile.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-import { Button } from 'react-bootstrap';
+import { Button, Col, Row } from 'react-bootstrap';
 
 const UserPublicProfile = props => {
   const { id, name, email, bio, bio_img, phone_number, website, instagram, location, services } = props.user;
@@ -14,13 +13,62 @@ const UserPublicProfile = props => {
 
   return (
     <div>
-      <div className="profile-bio-img" style={bioBackgroundImg}></div>
-      <Button href={`mailto:${email}`}>Email</Button>
-      <h2>{name}</h2>
-      <div>{renderServices}</div>
-      <div>{location}</div>
-      <div>{email}</div>
-      <div>{bio}</div>
+      <Row className="profile-header">
+        <Col sm={3}>
+          <div className="profile-bio-img" style={bioBackgroundImg}>
+            <Button href={`mailto:${email}`}><i className="fa fa-envelope-o"></i></Button>
+          </div>
+        </Col>
+        <Col sm={9} className="center-vertically">
+          <h1>Meet</h1>
+          <h2>{name}</h2>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col sm={2} className="center">
+          <i className="fa fa-briefcase profile-icon"></i>
+        </Col>
+        <Col sm={10}  className="border-col">
+          <div className="profile-section">
+            {renderServices}
+          </div>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col sm={2} className="center">
+          <i className="fa fa-map-marker profile-icon"></i>
+        </Col>
+        <Col sm={10}  className="border-col">
+          <div className="profile-section">
+            {location}
+          </div>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col sm={2} className="center">
+          <i className="fa fa-user profile-icon"></i>
+        </Col>
+        <Col sm={10}  className="border-col">
+          <div className="profile-section">
+            {bio}
+          </div>
+        </Col>
+      </Row>
+
+      <Row>
+      <Col sm={2} className="center">
+          <i className="fa fa-envelope profile-icon"></i>
+        </Col>
+        <Col sm={10}  className="border-col">
+          <div className="profile-section">
+            <div><a href={`mailto:${email}`}>{email}</a></div>
+            <div><a href={`tel:${phone_number}`}>{phone_number}</a></div>
+          </div>
+        </Col>
+      </Row>
     </div>
   )
 }
