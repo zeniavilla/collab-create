52cea778fa7576d8b5558eb53a59f91a3faeb3eb
Zenia <zeniavilla@hotmail.com>
2017-09-14 16:28:55 -0700

    Include Nav.css in NavCurrentUser component

--- client/src/containers/NavCurrentUser.js
+++ client/src/containers/NavCurrentUser.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import './Nav.css';
 import NavBar from '../components/NavBar';
 
 class NavCurrentUser extends Component {
