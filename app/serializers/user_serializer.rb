5eece60c88aec254ba29ca3cd31ebb546026fd05
Zenia <zeniavilla@hotmail.com>
2017-09-14 17:06:30 -0700

    Add :quote to user_serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :bio, :bio_img, :website, :phone_number, :instagram, :location, :dob, :likes, :liked
+  attributes :id, :name, :email, :bio, :bio_img, :website, :phone_number, :instagram, :location, :dob, :likes, :liked, :quote
   has_many :services, through: :user_services
 end
