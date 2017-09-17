7dd261fd4697a7f304bf4f44219bb306e42a9505
Zenia <zeniavilla@hotmail.com>
2017-09-16 21:12:47 -0700

    Skip :update action for devise users routes and add to :users

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
   scope '/api' do
-    devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :sessions => 'sessions', :registrations => 'registrations' }
+    devise_for :users, :skip => [:update], :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :sessions => 'sessions', :registrations => 'registrations' }
   end
 
   namespace :api do
     resources :services, except: [:new, :edit]
-    resources :users, only: [:show]
+    resources :users, only: [:show, :update]
   end
 
   root 'welcome#index'
