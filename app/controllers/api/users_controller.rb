fd5dd329416df433c0cf8deb9290d5a21776c0e1
Zenia <zeniavilla@hotmail.com>
2017-09-16 21:13:05 -0700

    Add update action to UsersController

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,4 +3,14 @@ class Api::UsersController < ApplicationController
     user = User.find_by(id: params[:id])
     render json: user
   end
+
+  def update
+    user = User.find_by(id: params[:id])
+    user.update_attributes(user_params)
+    render json: user
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :location, :website, :instagram, :phone_number, :bio, :bio_img, :quote, :service_ids => [])
+  end
 end
