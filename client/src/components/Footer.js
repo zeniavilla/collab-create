7fe1d91ef4afe43f92f7f74478c99bf081650788
Zenia <zeniavilla@hotmail.com>
2017-09-23 18:43:49 -0700

    Add Footer component

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
+import '../styles/components/Footer.css';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+
 const Footer = () =>
-  <div>
-  </div>
+  <Navbar className="footer">
+    <Navbar.Header>
+      <Navbar.Text>
+        <a className="footer-brand" href="#" title="Developed by Zenia Villa">© Collab-Create</a>
+      </Navbar.Text>
+    </Navbar.Header>
+
+    <Navbar.Collapse>
+      <Nav pullRight>
+        <NavItem href="https://github.com/zeniavilla/collab-create" target="_blank" title="View on GitHub">
+          <i className="fa fa-github" aria-hidden="true"></i>
+        </NavItem>
+        <NavItem href="https://zeniavilla.com" target="_blank" title="Visit ZeniaVilla.com">
+          <i className="fa fa-briefcase" aria-hidden="true"></i>
+        </NavItem>
+      </Nav>
+    </Navbar.Collapse>
+
+  </Navbar>
 
 export default Footer;
