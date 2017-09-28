8055e0d62b2f840b22ba8a1e06748ecc79c503bf
Zenia <zeniavilla@hotmail.com>
2017-09-27 17:48:30 -0700

    Remove unused links in NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -39,14 +39,6 @@ const NavBar = props => {
           <LinkContainer to="/services">
             <NavItem eventKey={1}>Services</NavItem>
           </LinkContainer>
-          <NavItem eventKey={2} href="#">Link</NavItem>
-          <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-            <MenuItem eventKey={3.1}>Action</MenuItem>
-            <MenuItem eventKey={3.2}>Another action</MenuItem>
-            <MenuItem eventKey={3.3}>Something else here</MenuItem>
-            <MenuItem divider />
-            <MenuItem eventKey={3.3}>Separated link</MenuItem>
-          </NavDropdown>
         </Nav>
         { (props.currentUser.id) ? userLinks : guestLinks }
       </Navbar.Collapse>
