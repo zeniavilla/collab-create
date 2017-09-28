c1510863d264c2bd7761b9d1acb44477d324a194
Zenia <zeniavilla@hotmail.com>
2017-09-27 18:27:43 -0700

    Remove unused components from NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 const NavBar = props => {
