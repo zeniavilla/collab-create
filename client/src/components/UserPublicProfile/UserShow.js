4ddca63b89b6f78c824cab3af72de9e505ae5426
Zenia <zeniavilla@hotmail.com>
2017-09-30 11:32:12 -0700

    Add getServices action to UserShow and map services to props

--- client/src/components/UserPublicProfile/UserShow.js
+++ client/src/components/UserPublicProfile/UserShow.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { getSelectedUserService } from '../../actions/userActions';
+import { getServices } from '../../actions/serviceActions';
 
 import UserPublicProfile from './UserPublicProfile';
+import ServicesNav from '../ServicesNav/ServicesNav';
 import '../../styles/components/UserShow.css';
 
 class UserShow extends Component {
@@ -11,19 +13,25 @@ class UserShow extends Component {
   componentDidMount = () => {
     const { userId } = this.props.match.params;
     this.props.getSelectedUserService(userId);
+    this.props.getServices();
   }
 
   render() {
     return (
-      <div className="clearfix">
-        <UserPublicProfile user={this.props.selectedUserService} />
+      <div>
+      <div id="wrapper" className="clearfix">
+        <UserPublicProfile user={this.props.selectedUserService} services={this.props.services} />
+      </div>
       </div>
     )
   }
 }
 
 const mapStateToProps = state => {
-  return ({ selectedUserService: state.selectedUserService })
+  return ({
+    selectedUserService: state.selectedUserService,
+    services: state.services
+  })
 }
 
-export default connect(mapStateToProps, { getSelectedUserService })(UserShow);
+export default connect(mapStateToProps, { getSelectedUserService, getServices })(UserShow);
