be3b787d52dbe592b0ef7d9e5be6994b19b6d003
Zenia <zeniavilla@hotmail.com>
2017-10-02 12:42:07 -0700

    Remove destroy route from devise users and add to /api/users

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
   scope '/api' do
-    devise_for :users, :skip => [:update], :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :sessions => 'sessions', :registrations => 'registrations' }
+    devise_for :users, :skip => [:update, :destroy], :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :sessions => 'sessions', :registrations => 'registrations' }
   end
 
   namespace :api do
     resources :services, except: [:new, :edit]
-    resources :users, only: [:show, :update]
+    resources :users, only: [:show, :update, :destroy]
   end
 
   root 'welcome#index'
