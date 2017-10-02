2777ec524889d74c7109d3e00b9035df0b4549a1
Zenia <zeniavilla@hotmail.com>
2017-10-01 20:47:26 -0700

    Add #wrapper and import UserForm css

--- client/src/components/UserEdit/UserEditForm.js
+++ client/src/components/UserEdit/UserEditForm.js
@@ -1,66 +1,76 @@
 import React from 'react';
 
+// styles
+import '../../styles/components/UserForm.css';
+
+// components
+import { Col } from 'react-bootstrap';
+
 const UserEditForm = props => {
   const { handleOnChange, handleOnSubmit, currentUser, services } = props;
   const { name, email, password, password_confirmation, bio, bio_img, location, phone_number, website, instagram } = props.userFormData;
 
   return (
-    <form onSubmit={handleOnSubmit}>
-      <div className="form-field">
-        <input type="text" id="name" placeholder={currentUser.name} onChange={handleOnChange} value={name} />
-      </div>
-
-      <div className="form-field">
-        <input type="email" id="email" placeholder={currentUser.email} onChange={handleOnChange} value={email} />
-      </div>
-
-      <div className="form-field">
-          <input type="password" id="password" placeholder="New password" onChange={handleOnChange} value={password} />
+    <div id="wrapper">
+      <form onSubmit={handleOnSubmit}>
+        <div className="form-field">
+          <input type="text" id="name" placeholder={currentUser.name} onChange={handleOnChange} value={name} />
         </div>
 
         <div className="form-field">
-          <input type="password" id="password_confirmation" placeholder="Confirm password" onChange={handleOnChange} value={password_confirmation} />
+          <input type="email" id="email" placeholder={currentUser.email} onChange={handleOnChange} value={email} />
         </div>
 
         <div className="form-field">
-          <input type="text" id="bio" placeholder={currentUser.bio} onChange={handleOnChange} value={bio} />
-        </div>
+            <input type="password" id="password" placeholder="New password" onChange={handleOnChange} value={password} />
+          </div>
 
-        <div className="form-field">
-          <input type="text" id="bio_img" placeholder={currentUser.bio_img} onChange={handleOnChange} value={bio_img} />
-        </div>
+          <div className="form-field">
+            <input type="password" id="password_confirmation" placeholder="Confirm password" onChange={handleOnChange} value={password_confirmation} />
+          </div>
 
-        <div className="form-field">
-          <input type="text" id="location" placeholder={currentUser.location} onChange={handleOnChange} value={location} />
-        </div>
+          <div className="form-field">
+            <input type="text" id="bio" placeholder={currentUser.bio} onChange={handleOnChange} value={bio} />
+          </div>
 
-        <div className="form-field">
-          <input type="text" id="phone_number" placeholder={currentUser.phone_number} onChange={handleOnChange} value={phone_number} />
-        </div>
+          <div className="form-field">
+            <input type="text" id="bio_img" placeholder={currentUser.bio_img} onChange={handleOnChange} value={bio_img} />
+          </div>
 
-        <div className="form-field">
-          <input type="text" id="website" placeholder={currentUser.website} onChange={handleOnChange} value={website} />
-        </div>
+          <div className="form-field">
+            <input type="text" id="location" placeholder={currentUser.location} onChange={handleOnChange} value={location} />
+          </div>
 
-        <div className="form-field">
-          <input type="text" id="instagram" placeholder={currentUser.instagram} onChange={handleOnChange} value={instagram} />
-        </div>
+          <div className="form-field">
+            <input type="text" id="phone_number" placeholder={currentUser.phone_number} onChange={handleOnChange} value={phone_number} />
+          </div>
 
-        <div className="form-field">
+          <div className="form-field">
+            <input type="text" id="website" placeholder={currentUser.website} onChange={handleOnChange} value={website} />
+          </div>
+
+          <div className="form-field">
+            <input type="text" id="instagram" placeholder={currentUser.instagram} onChange={handleOnChange} value={instagram} />
+          </div>
+
+          <div className="form-field-check clearfix">
           {
             services.map(service => 
-              <span key={service.id} className="form-checkboxes">
-                {service.name} 
-                <input type="checkbox" name={service.name} value={service.id} onChange={handleOnChange} />
-              </span>
+              <Col sm={4}>
+                <span className="form-checkboxes">
+                  {service.name} 
+                  <input type="checkbox" name={service.name} value={service.id} onChange={handleOnChange} />
+                </span>
+              </Col>
             )
           }
         </div>
 
-        <div className="form-action">
-          <button className="btn-primary">Edit</button>
-        </div>
-    </form>
+          <div className="form-action">
+            <button className="btn-form">Save</button>
+          </div>
+      </form>
+    </div>
   )
 }
 
