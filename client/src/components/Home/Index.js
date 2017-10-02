0e3f69b6b83a8c528a35c87a9c712722a1dcff69
Zenia <zeniavilla@hotmail.com>
2017-10-02 16:33:16 -0700

    Initialize Index component

---
+++ client/src/components/Home/Index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Switch } from 'react-router-dom';
+
+// styles
+import '../../styles/components/Index.css';
+
+class Index extends Component {
+  render() {
+    return (
+      <Switch>
+        <div className="home-background">
+        </div>
+      </Switch>
+    )
+  }
+}
+
+export default Index;
