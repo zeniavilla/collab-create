10b93bac9f062e40cfa1f946b04562ecf8970861
Zenia <zeniavilla@hotmail.com>
2017-10-03 09:39:05 -0700

    Move input errors above input in UserSignUpForm

--- client/src/components/SignUpForm/UserSignUpForm.js
+++ client/src/components/SignUpForm/UserSignUpForm.js
@@ -18,57 +18,69 @@ const UserSignUpForm = props => {
       <h2>Create account</h2>
         <label htmlFor="name">Full name</label>
         <div className="form-field">
-          <input type="text" id="name" placeholder={(errors) ? errors.name : null} onChange={handleOnChange} value={name || ''} />
+          <div className="form-error">
+          { (errors) ? errors.name : null }
+          </div>
+          <input type="text" id="name" onChange={handleOnChange} value={name || ''} />
         </div>
         
         <label htmlFor="email">Email</label>
         <div className="form-field">
-          <input type="email" id="email" placeholder={(errors) ? errors.email : null} onChange={handleOnChange} value={email || ''} />
+          <div className="form-error">
+          { (errors) ? errors.email : null }
+          </div>
+          <input type="email" id="email" onChange={handleOnChange} value={email || ''} />
         </div>
         
         <label htmlFor="password">Password</label>
         <div className="form-field">
-          <input type="password" id="password" placeholder={(errors) ? errors.password : null} onChange={handleOnChange} value={password || ''} />
+          <div className="form-error">
+          { (errors) ? errors.password : null }
+          </div>
+          <input type="password" id="password" onChange={handleOnChange} value={password || ''} />
         </div>
 
         <label htmlFor="password_confirmation">Confirm password</label>
         <div className="form-field">
+          <div className="form-error">
+          { (errors) ? errors.password_confirmation : null }
+          </div>
           <input type="password" id="password_confirmation" onChange={handleOnChange} value={password_confirmation || ''} />
         </div>
 
         <label htmlFor="bio">Introduce yourself</label>
         <div className="form-field">
-          <textarea id="bio" placeholder={(errors) ? errors.bio : null} onChange={handleOnChange} value={bio || ''} />
+          <textarea id="bio" onChange={handleOnChange} value={bio || ''} />
         </div>
 
         <label htmlFor="bio_img">Profile image URL</label>
         <div className="form-field">
-          <input type="text" id="bio_img" placeholder={(errors) ? errors.bio_img : null} onChange={handleOnChange} value={bio_img || ''} />
+          <input type="text" id="bio_img" onChange={handleOnChange} value={bio_img || ''} />
         </div>
 
         <label htmlFor="location">Location</label>
         <div className="form-field">
-          <input type="text" id="location" placeholder={(errors) ? errors.location : null} onChange={handleOnChange} value={location || ''} />
+          <input type="text" id="location" onChange={handleOnChange} value={location || ''} />
         </div>
 
         <label htmlFor="phone_number">Phone number</label>
         <div className="form-field">
-          <input type="text" id="phone_number" placeholder={(errors) ? errors.phone_number : null} onChange={handleOnChange} value={phone_number || ''} />
+          <input type="text" id="phone_number" onChange={handleOnChange} value={phone_number || ''} />
         </div>
 
         <label htmlFor="website">Website</label>
         <div className="form-field">
-          <input type="text" id="website" placeholder={(errors) ? errors.website : null} onChange={handleOnChange} value={website || ''} />
+          <input type="text" id="website" value={website || ''} />
         </div>
 
         <label htmlFor="instagram">Instagram username</label>
         <div className="form-field">
-          <input type="text" id="instagram" placeholder={(errors) ? errors.instagram : null} onChange={handleOnChange} value={instagram || ''} />
+          <input type="text" id="instagram" value={instagram || ''} />
         </div>
 
         <label htmlFor="quote">Quote</label>
         <div className="form-field">
-          <input type="text" id="quote" placeholder={(errors) ? errors.quote : null} onChange={handleOnChange} value={quote || ''} />
+          <input type="text" id="quote" value={quote || ''} />
         </div>
 
         <label>Select services</label>
