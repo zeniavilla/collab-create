7ac1b75b73c51e4f266b6c304ab1b34a8f20e474
Zenia <zeniavilla@hotmail.com>
2017-10-02 17:51:46 -0700

    Add title, body text, and button to Index component

--- client/src/components/Home/Index.js
+++ client/src/components/Home/Index.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
-import { Switch } from 'react-router-dom';
+import React from 'react';
 
 // styles
 import '../../styles/components/Index.css';
 
-class Index extends Component {
-  render() {
-    return (
-      <Switch>
-        <div className="home-background">
-        </div>
-      </Switch>
-    )
-  }
+// components
+import { LinkContainer } from 'react-router-bootstrap';
+
+const Index = () => {
+  return (
+    <div className="home-background">
+      <h2>Create something spectacular</h2>
+      <div className="home-body">
+        collab|create provides a network of creative professionals throughout the world. Whether you're looking to connect with a fashion expert, build your portfolio, or collaborate on fun projects, you'll find the best team of creatives.
+      </div>
+      <LinkContainer to="/signup" className="btn-announcement">
+        <button>Get started</button>
+      </LinkContainer>
+    </div>
+  )
 }
 
 export default Index;
